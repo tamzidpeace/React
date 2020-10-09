a1e9f38195e1247187f589ae7f0c96715bbaaab0
Arafat Kamal <tamjedpeace@gmail.com>
2020-10-09 08:26:05 +0600

    create re useable component

--- app00/src/App.js
+++ app00/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> HI I am react app. </h1>
+        <h1> Hi I am react app. </h1>
+         <Person/>
       </div>
     );
   }

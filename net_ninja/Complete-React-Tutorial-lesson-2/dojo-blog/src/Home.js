d7e00bd566163592c3e68128d9d845aa3d201ee1
Arafat Kamal <tamjedpeace@gmail.com>
2021-05-14 21:44:07 +0600

    button click

--- net_ninja/Complete-React-Tutorial-lesson-2/dojo-blog/src/Home.js
+++ net_ninja/Complete-React-Tutorial-lesson-2/dojo-blog/src/Home.js
@@ -1,7 +1,20 @@
-const Home= () => {
+const Home = () => {
+
+    const handleClick = () => {
+        console.log('hello, ninjas')
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello, ' + name)
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => {
+                handleClickAgain('arafat')
+            }}>Click Me Again</button>
         </div>
     )
 }

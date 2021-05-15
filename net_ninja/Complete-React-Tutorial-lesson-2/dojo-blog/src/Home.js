17fb1c44deb16a9dd09b4661e43a8a8250c9758e
Arafat Kamal <tamjedpeace@gmail.com>
2021-05-15 12:22:06 +0600

    use of json server

--- net_ninja/Complete-React-Tutorial-lesson-2/dojo-blog/src/Home.js
+++ net_ninja/Complete-React-Tutorial-lesson-2/dojo-blog/src/Home.js
@@ -1,13 +1,9 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
-        {title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
-        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
-    ])
+    const [blogs, setBlogs] = useState(null)
 
     const title = "All Blogs"
 
@@ -16,9 +12,20 @@ const Home = () => {
         setBlogs(new_blogs)
     }
 
+    //fetching data from json server
+    useEffect(() => {
+        fetch('http://localhost:3001/blogs')
+            .then(res => {
+                return res.json()
+            }).then(data => {
+            console.log(data)
+            setBlogs(data)
+        })
+    }, [])
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title={title} handleDelete={handleDelete}/>
+            {blogs && <BlogList blogs={blogs} title={title} handleDelete={handleDelete}/>}
         </div>
     )
 }

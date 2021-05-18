2b7dda6246c02da68795b2a813358f728c18d1ce
Arafat Kamal <tamjedpeace@gmail.com>
2021-05-18 10:06:28 +0600

    add header and footer component

--- arafat-portfolio/src/App.js
+++ arafat-portfolio/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Footer from "./Components/Footer";
+import Header from './Components/Header'
 
 function App() {
     return (
         <div className="App">
 
-            <h1>Hello world</h1>
+            {/*mobile navigation button*/}
+            <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+            <Header/>
+
+            <Footer/>
 
         </div>
     );
---
+++ arafat-portfolio/src/Components/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+    return (
+        <div>
+
+            <footer id="footer">
+                <div className="container">
+                    <div className="copyright">
+                        &copy; Copyright <strong><span>iPortfolio</span></strong>
+                    </div>
+                    <div className="credits">
+                        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                    </div>
+                </div>
+            </footer>
+
+        </div>
+    )
+}
+
+export default Footer

bee29fb8d2d2be524bd03d67227ab2623e6e68fc
tamzidpeace <tamjedpeace@gmail.com>
2021-05-18 20:05:46 +0600

    all necessary components added

--- arafat-portfolio/src/App.js
+++ arafat-portfolio/src/App.js
@@ -3,6 +3,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Footer from "./Components/Footer";
 import Header from './Components/Header'
 import Hero from "./Components/Hero";
+import About from "./Components/About";
+import Skills from "./Components/Skills";
+import Resume from "./Components/Resume";
+import Portfolio from "./Components/Portfolio";
+import Contact from "./Components/Contact";
 
 function App() {
     return (
@@ -12,8 +17,14 @@ function App() {
             <button type="button" className="mobile-nav-toggle d-xl-none">
                 <i className="icofont-navigation-menu"></i>
             </button>
+
             <Header/>
             <Hero/>
+            <About/>
+            <Skills/>
+            <Resume/>
+            <Portfolio/>
+            <Contact/>
             <Footer/>
 
         </div>

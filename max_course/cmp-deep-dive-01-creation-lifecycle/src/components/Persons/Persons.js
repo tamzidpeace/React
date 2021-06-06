efa6ddb47344c8b8928703c34b8c13f0e33be541
Arafat Kamal <tamjedpeace@gmail.com>
2021-06-06 10:41:56 +0600

    max course component props advanced

---
+++ max_course/cmp-deep-dive-01-creation-lifecycle/src/components/Persons/Persons.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+
+import Person from './Person/Person';
+
+
+class Persons  extends Component{
+
+  constructor(props) {
+    super(props);
+    console.log('Persons.js constructor')
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('Persons.js getDerivedStateFromProps')
+    return state;
+  }
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    console.log('Persons.js shouldComponentUpdate')
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('Persons.js getSnapshotBeforeUpdate');
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('Persons.js componentDidUpdate')
+  }
+
+  render() {
+    console.log('[Persons.js] rendering...');
+    return this.props.persons.map((person, index) => {
+      return (
+          <Person
+              click={() => this.props.clicked(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={event => this.props.changed(event, person.id)}
+          />
+      );
+    });
+  }
+
+};
+
+export default Persons;

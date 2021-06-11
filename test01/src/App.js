4dccc77a984f6a3a855f381c27b878ed3f53bfe8
Arafat Kamal <tamjedpeace@gmail.com>
2021-06-11 18:46:02 +0600

    redux setup

--- test01/src/App.js
+++ test01/src/App.js
@@ -1,31 +1,17 @@
 import React from "react";
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import Counter from "./counter/counter.jsx";
+
 
 
 export default function App() {
 
+
+
+
     return (
         <React.Fragment>
 
-            <div style={{textAlign: 'center',}}>
-                <h1>Redux Made Easy</h1>
-
-                <div className="count">
-
-                    <h3>Count : 0</h3>
-
-                    <div className="mt-5">
-                        <button className="btn btn-primary mr-2">
-                            <i className="fas fa-plus"/>
-                        </button>
-                        <button className="btn btn-primary mr-2">
-                            <i className="fas fa-minus"/>
-                        </button>
-                    </div>
-                </div>
-
-            </div>
+        <Counter/>
 
         </React.Fragment>
     );

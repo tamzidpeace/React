6ecd5310b683a113bb5706522009b3ca6b60efd8
Arafat Kamal <tamjedpeace@gmail.com>
2021-06-12 10:05:21 +0600

    simple changes'

--- test01/src/App.js
+++ test01/src/App.js
@@ -9,8 +9,6 @@ export default function App() {
 
             <Counter/>
 
-            <Counter2/>
-
         </React.Fragment>
     );
 }
--- test01/src/counter/counter2.js
+++ test01/src/counter/counter2.js
@@ -4,7 +4,8 @@ import {decrement2, increment2} from "../redux/ducks/counter";
 export default function Counter2() {
 
 
-    const count = useSelector((state) => state.counter.count2);
+    const count2 = useSelector((state) => state.counter.count2);
+    const count = useSelector((state) => state.counter.count);
     const dispatch = useDispatch();
 
     const incrementHandler = () => {
@@ -18,6 +19,7 @@ export default function Counter2() {
     return (
         <div>
             <h1>count {count}</h1>
+            <h1>count2 {count2}</h1>
 
             <button onClick={decrementHandler}>minus</button>
             <button onClick={incrementHandler}>plus</button>

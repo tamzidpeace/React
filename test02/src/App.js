81ae629b0db361d9df1aa7de006bf9fdceb93f3a
Arafat Kamal <tamjedpeace@gmail.com>
2021-06-18 12:20:01 +0600

    simple orientation

--- test02/src/App.js
+++ test02/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <div className="container App">
 
             <Login/>
+
             <Profile/>
 
         </div>
--- test02/src/Components/Login.jsx
+++ test02/src/Components/Login.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Login(props) {
+
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const handleEmail = (event) => {
+        let email = event.target.value;
+        setEmail(email);
+        console.log(email);
+    }
+
     return (
         <div className="p-5">
 
             <h3 className="text-center mb-5">Login Form</h3>
             <div className="d-flex flex-row justify-content-center">
-                <form action="" className="loginForm mx-5">
+                <form action="#" className="loginForm mx-5">
                     <label htmlFor="">Email</label>
-                    <input type="email" className="form-control" name="email"/>
+                    <input onChange={handleEmail} type="email" className="form-control" name="email" />
 
                     <label className="mt-3" htmlFor="">Password</label>
                     <input type="password" className="form-control" name="password"/>
 
-                    <button style={{'float': "right"}} className="btn btn-primary btn-block mt-3">Login</button>
+                    <button  style={{'float': "right"}} className="btn btn-primary btn-block mt-3">Login</button>
                 </form>
             </div>
 

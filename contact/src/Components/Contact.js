3ef976353a496725a75d2198220012b126c4795c
Arafat Kamal <tamjedpeace@gmail.com>
2021-06-22 08:49:15 +0600

    contact add process is running

--- contact/src/Components/Contact.js
+++ contact/src/Components/Contact.js
@@ -3,11 +3,11 @@ import React from 'react';
 function Contact(props) {
     return (
         <div className="contact my-3 d-flex flex-row justify-content-between">
-            <div>Name</div>
-            <div>1234567879</div>
+            <div>{props.name}</div>
+            <div>{props.pone}</div>
             <div>
-                <i className="mx-2 fas fa-edit text-info"></i>
-                <i className="fas fa-trash text-danger"></i>
+                <i className="mx-2 fas fa-edit text-info"/>
+                <i className="fas fa-trash text-danger"/>
             </div>
         </div>
     );
--- contact/src/Components/Contacts.js
+++ contact/src/Components/Contacts.js
@@ -1,25 +1,103 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Contact from "./Contact";
 import "bootstrap/dist/css/bootstrap.css"
+import Modal from "react-bootstrap/Modal";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Contacts(props) {
+
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    const showModal = () => {
+        setIsOpen(true);
+    };
+
+    const hideModal = () => {
+        setIsOpen(false);
+    };
+
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+
+    const handleNameChange = (event) =>{
+        //console.log(event.target.value);
+        setName(event.target.value);
+
+    }
+
+    const handlePhoneChange =(event) =>{
+        //console.log(event.target.value);
+        setPhone(event.target.value);
+    }
+
+    const saveContact = () => {
+        hideModal();
+        let data = {'name': name, 'phone': phone};
+
+        cnts = [data , ...cnts];
+        setContact(cnts);
+    }
+
+    let cnts = [
+        {'name': 'arafat', 'phone': 1234},
+        {'name': 'saif', 'phone': 4321},
+    ];
+
+    const [contacts, setContact] = useState(cnts);
+
     return (
         <div className="container">
 
             <div className="d-flex flex-row justify-content-center mt-5">
                 <div className="card" style={{width: "30rem",}}>
 
-                    <div className="card-body">
-
+                    <div className="card-header">
                         <h1 className="card-title">Contact List</h1>
 
-                        <Contact/>
-                        <Contact/>
-                        <Contact/>
+                        <div className="card-toolbar">
+
+                            <div className="float-end mb-2">
+                                <button className="btn btn-primary" onClick={showModal}>
+                                    Add New <i className="fas fa-plus-circle"/>
+                                </button>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div className="card-body">
+
+                        {contacts.map(contact => <Contact key={contact.name} name={contact.name} pone={contact.phone}/> )}
 
                     </div>
                 </div>
             </div>
+
+
+            <>
+                <Modal animation={false} show={isOpen} onHide={hideModal}>
+                    <Modal.Header>
+                        <Modal.Title>Add New Contact</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+
+                        <label>Name</label>
+                        <input type="text" name="name" value={name} onChange={handleNameChange} className="form-control"/>
+
+                        <label>Phone Number</label>
+                        <input type="number" name="phone" value={phone} onChange={handlePhoneChange} className="form-control"/>
+
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <button className="btn btn-dark" onClick={hideModal}>Cancel</button>
+                        <button className="btn btn-primary" onClick={saveContact}>Save</button>
+                    </Modal.Footer>
+                </Modal>
+            </>
+
+
+
 
         </div>
     );
